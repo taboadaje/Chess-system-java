e3e394d72215632a2de16622dd736151e89e7d0c
taboadaje <taboadaje@gmail.com>
2024-02-20 14:29:05 -0300

    Moving pieces

--- chess-system/src/application/Main.java
+++ chess-system/src/application/Main.java
@@ -3,6 +3,10 @@ package application;
 import  boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,9 +16,23 @@ public class Main {
 
         //Board board = new Board(8, 8);
 
-        ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
-        
+        Scanner sc = new Scanner(System.in);
 
+        ChessMatch chessMatch = new ChessMatch();
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+
+        }
     }
 }
