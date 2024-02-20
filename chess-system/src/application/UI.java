df1066fcc934f4b2132f502ef6b670384be9ac82
taboadaje <taboadaje@gmail.com>
2024-02-20 11:46:05 -0300

    BoardException and defensive programming

--- chess-system/src/application/UI.java
+++ chess-system/src/application/UI.java
@@ -6,13 +6,13 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
+            System.out.print((8 - i) + "  ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j]);
             }
             System.out.println();
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println("   a  b  c  d  e  f  g  h");
 
     }
 
@@ -21,8 +21,8 @@ public class UI {
             System.out.print("-");
         }
         else {
-            System.out.println(piece);
+            System.out.print(piece);
         }
-        System.out.print(" ");
+        System.out.print("  ");
     }
 }
