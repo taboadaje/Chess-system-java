6ba07bf61037019bcc0589b7675073557ba0ae4c
taboadaje <taboadaje@gmail.com>
2024-02-22 10:17:22 -0300

    Switching player each turn

--- chess-system/src/application/Main.java
+++ chess-system/src/application/Main.java
@@ -25,7 +25,8 @@ public class Main {
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
+                //UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
